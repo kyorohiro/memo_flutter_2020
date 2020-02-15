34e063652115864d69f3078de0a6af5760519c85
kyorohiro <kyorohiro@gmail.com>
2020-02-15 11:28:07 +0900

    fc

---
+++ android/app/src/main/java/info/kyorohiro/memo_flutter_2020/MainActivity.java
@@ -0,0 +1,13 @@
+package info.kyorohiro.memo_flutter_2020;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
